d6efe8273e0a7133d285e9d2834a78e26afb94c7
tuandiep <tuandiep98usa@gmail.com>
2022-03-30 00:20:02 +0700

    #init swift code

---
+++ MovieTicket/MovieTicket/MovieTicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTicketApp.swift
+//  MovieTicket
+//
+//  Created by Tuấn Điệp on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieTicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieTicket/MovieTicket/UI/BottomNavBar.swift
@@ -0,0 +1,20 @@
+//
+//  BottomNavBar.swift
+//  MovieTicket
+//
+//  Created by Tuấn Điệp on 30/03/2022.
+//
+
+import SwiftUI
+
+struct BottomNavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomNavBar_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomNavBar()
+    }
+}
